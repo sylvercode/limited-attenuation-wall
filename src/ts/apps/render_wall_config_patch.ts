5d71e6627b880140929446d85fb0370e6b6edee6
Sylvercode <sylver.dev@outlook.com>
2025-08-29 20:10:56 -0400

    render WallConfig fields with DataSchema

--- src/ts/apps/render_wall_config_patch.ts
+++ src/ts/apps/render_wall_config_patch.ts
@@ -1,63 +1,46 @@
 import { HookDefinitions } from "fvtt-hook-attacher";
 import ApplicationV2 from "node_modules/fvtt-types/src/foundry/client/applications/api/application.mjs";
 import { MODULE_ID } from "../constants";
+import { DataField } from "node_modules/fvtt-types/src/foundry/common/data/fields.mjs";
+import { LimitedAttenuationWallFlags, LimitedAttenuationWallFlagsDataModel } from "./limited_attenuation_wall_flags_data_model";
 
-const ATTENUATION_RATIO_FIELD = "attenuationRatio";
 
-declare module "fvtt-types/configuration" {
-    interface FlagConfig {
-        Wall: {
-            [MODULE_ID]?: {
-                [ATTENUATION_RATIO_FIELD]?: number;
-            };
-        };
-    }
-}
-
-export class WallConfigPatch {
-
-    static getWallFlags(wallDoc: WallDocument): FlagConfig["Wall"] {
-        const data = {
-            sylvercodeEnhanceLimitedWall: {
-                attenuationRatio: wallDoc.getFlag(MODULE_ID, ATTENUATION_RATIO_FIELD) ?? 0.5
-            }
-        };
-        return data;
-    }
-
-    static async render(
-        application: WallConfig,
-        element: HTMLElement,
-        context: ApplicationV2.RenderContextOf<WallConfig>,
-        options: ApplicationV2.RenderOptionsOf<WallConfig>) {
-        console.log("RenderWallConfigPatch: OnRenderWallConfig");
-        console.log({ application, element, context, options });
-
-        const wallFlags = WallConfigPatch.getWallFlags(context.document);
-
-        const newDivHtml = await foundry.applications.handlebars.renderTemplate(
-            `modules/${MODULE_ID}/templates/limited_attenuation_wall_config.hbs`,
-            { flags: wallFlags });
-
-        const content = element.querySelector(".window-content");
-        if (!content) {
-            console.error("Could not find .window-content element");
-            return;
-        }
-
-        const fieldset = content.querySelector('fieldset:has(select[name="sight"])');
-        if (!fieldset) {
-            console.error('Could not find fieldset containing select[name="sight"]');
-            return;
-        }
-
-        fieldset.insertAdjacentHTML('beforeend', newDivHtml);
-    }
-}
-
-export const HOOKS_DEFINITIONS: HookDefinitions = {
-    on: {
+export const HOOKS_DEFINITIONS: Iterable<HookDefinitions> = [{
+    on: [{
         name: "renderWallConfig",
-        callback: WallConfigPatch.render
+        callback: renderWallConfig
+    }]
+}];
+
+async function renderWallConfig(
+    _application: WallConfig,
+    element: HTMLElement,
+    context: ApplicationV2.RenderContextOf<WallConfig>,
+    _options: ApplicationV2.RenderOptionsOf<WallConfig>) {
+
+    const dataModel = new LimitedAttenuationWallFlagsDataModel(context.document);
+    const fieldsNames: (keyof LimitedAttenuationWallFlags)[] = ["hasLimitedAttenuation", "limitedAttenuationRatio"];
+    const fields = fieldsNames.map(fieldName => {
+        const field = dataModel.schema.fields[fieldName] as DataField<any, any>;
+        return field.toFormGroup(
+            { rootId: `${context.rootId}-${MODULE_ID}-${fieldName}` },
+            { value: dataModel[fieldName] }
+        );
+    });
+
+    const content = element.querySelector(".window-content");
+    if (!content) {
+        console.error("Could not find .window-content element");
+        return;
     }
-};
+
+    const fieldset = content.querySelector('fieldset:has(select[name="sight"])');
+    if (!fieldset) {
+        console.error('Could not find fieldset containing select[name="sight"]');
+        return;
+    }
+
+    fields.forEach(field => {
+        fieldset.append(field);
+    });
+}
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -3,6 +3,7 @@ import * as dogBrowserApp from "./apps/dog_browser";
 import type { LibWrapperWrapperDefinitions } from "fvtt-lib-wrapper-types";
 import { HookDefinitions } from "fvtt-hook-attacher";
 import * as RenderWallConfigPatchApp from "./apps/render_wall_config_patch";
+import { LimitedAttenuationWallFlagsDataModel } from "./apps/limited_attenuation_wall_flags_data_model";
 
 
 export interface SylvercodeEnhanceLimitedWallModule
@@ -17,12 +18,13 @@ export class SylvercodeEnhanceLimitedWallModuleHooks {
     dogBrowserApp.onInitHandle,
   ];
 
-  static LIBWRAPPER_PATCHS: Iterable<LibWrapperWrapperDefinitions> = Array.from(
-    ClockwiseSweepPolygonPatch.LIBWRAPPER_PATCHS,
-  );
+  static LIBWRAPPER_PATCHS: Iterable<LibWrapperWrapperDefinitions> = [
+    ...ClockwiseSweepPolygonPatch.LIBWRAPPER_PATCHS,
+  ];
 
   static HOOKS_DEFINITIONS_SET: HookDefinitions[] = [
     dogBrowserApp.HOOKS_DEFINITIONS,
-    RenderWallConfigPatchApp.HOOKS_DEFINITIONS,
+    ...LimitedAttenuationWallFlagsDataModel.HOOKS_DEFINITIONS,
+    ...RenderWallConfigPatchApp.HOOKS_DEFINITIONS,
   ]
 }
